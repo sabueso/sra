47e2a9b24f392804b1a9ab88766369901a1d0450
sabueso <sabueso@veriton>
2013-06-20 22:58:38 +0200

    hilos funcionando...

--- src/performance.py
+++ src/performance.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
-import threading
+import threading,ssh_cliente,commands
+import sra
 
-def comandociclo():
-	print str("hola")
-
-def ejecucion():
-	t = threading.Timer(5, comandociclo)
-	t.start()
+def uptimestate():
+	#commands.getstatusoutput("uptime")
+	#print "hola"
+	status2 = self.builder.get_object('statusbar2')
+	status2.push(1,""+ssh_cliente.comando("uptime")+"")
+	threading.Timer(3,uptimestate).start()
+	return
